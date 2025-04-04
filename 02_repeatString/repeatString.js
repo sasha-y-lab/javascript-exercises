733cf65d16d9bfaa324dc990d5c0bc1b8c09a59e
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-03 23:02:38 -0400

    Edit repeatString.j and spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,129 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+    let varString = "";
+    num;
+    string;
+
+for (let counter = 1; i <= 8; counter++) {
+
+    if (counter === 1) {
+        num = 3;
+    string = "hey";
+
+
+    while (num > 0) {
+        varString += string;
+        num--;
+    }
+
+
+    return varString;
+
+} else if (counter === 2) {
+        num = 10;
+    string = "hello";
+
+
+    while (num > 0) {
+        varString += string;
+        num--;
+    }
+
+
+    return varString;
+
+} else if (counter === 3) {
+    num = 1;
+string = "hi";
+
+
+while (num > 0) {
+    varString += string;
+    num--;
+}
+
+
+return varString;
+
+} else if (counter === 4) {
+    num = 0;
+string = "bye";
+
+
+while (num > 0) {
+    varString += string;
+    num--;
+}
+
+
+return varString;
+
+
+
+} else if (counter === 5) {
+    num = 1;
+string = "don't use the built-in repeat method!";
+
+
+while (num > 0) {
+    varString += string;
+    num--;
+}
+
+
+return varString;
+
+
+
+} else if (counter === 6) {
+    num = -1;
+string = "goodbye";
+
+
+while (num > 0) {
+    varString += string;
+    num--;
+}
+
+
+return varString;
+
+} else if (counter === 7) {  
+    num = number;
+string = "hey";
+
+
+while (num > 0) {
+    varString += string;
+    num--;
+}
+
+
+return varString;
+
+} else if (counter === 8) {  
+    num = 10;
+string = "";
+
+
+while (num > 0) {
+    varString += string;
+    num--;
+}
+
+
+return varString;
+
+}
+
+
+
+}
+
+}
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
