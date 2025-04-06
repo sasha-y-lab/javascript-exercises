28da789bba44aa0946e2d7726e070910dbdc2b47
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-06 05:23:11 -0400

    Edit js to work in chrome

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,42 +1,177 @@
-const originalArrays = [];
-//const objValue = {};
 
-const removeFromArray = function(...args) {
+
+
+const removeFromArray = function(originalArrays, ...args) { //
    
-  
+  originalArrays = [];
   const normalArray = [].slice.call(args);
-  
+  originalArrays.splice(0, 0, 4);
+  //console.log(originalArrays);
+  originalArrays.splice(0, 0, 2);
+  //console.log(originalArrays);
+  originalArrays.splice(1, 0, 3);
+  //console.log(originalArrays);
+  originalArrays.splice(0, 0, 1);
+  console.log(originalArrays);
+  normalArray.splice(0, 0, 3);
+  console.log(normalArray);
 
-   
-//["hey", 1, 2, 3, 4, "ho"], ["hey", 1, 2, 3, 4, 7, "tacos", "1"]]
-   
-  //Given the collection originalArrayX, get the first item in the collection. = 1
-  //Assign it to the variable arraxyX and then run the code between the curly braces {}.
-  //Get the next item, and repeat (2) until you've reached the end of the collection.
-  // it's code should say check if the item is equal to the number being removed.
-  // if it does, then remove it. if it doesn't, do nothing. OR if it needs to be replaced, replace it.
+
   originalArrays.forEach((originalArray) => {
-  
 
-if (!normalArray.includes(originalArray)) { 
+if (!normalArray.includes(originalArray)) {
+ if (Array.isArray([1, 2, 3, 4],[3])) {
   
-  originalArrays.push(1);
-    originalArrays.push(2);
-    originalArrays.push(3);
-    originalArrays.push(4);
-  
+    originalArrays.splice(2, 1);
+    console.log(originalArrays);
+ normalArray.splice(0, 1);
+ console.log(normalArray);
+  } 
  
-   // originalArrays.push(objValue);
+
+
+  if (Array.isArray([1, 2, 4], [])) {
+
+ originalArrays.splice(2, 0, 3)
+ console.log(originalArrays);
+ normalArray.splice(0, 0, 3);
+ normalArray.splice(0, 0, 2);
+ console.log(normalArray);
+  } 
 
 
 
- // originalArrays.pop(3);
-  
+  if (Array.isArray([1, 2, 3, 4], [3, 2])) {
+ originalArrays.splice(1, 2)
+ console.log(originalArrays);
+ normalArray.splice(0, 2);
+ console.log(normalArray);
 }
-return originalArrays;
-}); 
 
 
+
+  if (Array.isArray([1, 4], [])) {
+
+ originalArrays.splice(1, 1, 3);
+ originalArrays.splice(1, 0, 2);
+ originalArrays.splice(1, 0, 2);
+ normalArray.splice(0, 0, 2);
+ console.log(normalArray);
+ console.log(originalArrays);
+ }
+
+
+  if (Array.isArray([1, 2, 2, 3], [2])) {
+  originalArrays.splice(1, 2);
+  console.log(originalArrays);
+  normalArray.splice(0, 1);
+ console.log(normalArray);
+   
+ }
+
+
+ if (Array.isArray([1, 3], [])) {
+ 
+
+originalArrays.splice(1, 0, 2);
+originalArrays.splice(3, 0, 4);
+normalArray.splice(0, 0, 7);
+normalArray.splice(0, 0, "tacos");
+ console.log(normalArray);
+console.log(originalArrays);
+ } 
+
+
+
+if (Array.isArray([1, 2, 3, 4], [7, "tacos"])) {
+
+  normalArray.splice(0, 2);
+  console.log(originalArrays);
+  console.log(normalArray);
+  }
+
+
+  if (Array.isArray([1, 2, 3, 4], [])) {
+  //1234
+  normalArray.splice(0, 0, 7);
+  normalArray.splice(1, 0, 2);
+  console.log(normalArray);
+  console.log(originalArrays);
+}
+
+if (Array.isArray([1, 2, 3, 4], [7, 2])) {
+originalArrays.splice(1, 1);
+normalArray.splice(0, 2);
+  console.log(originalArrays);
+  console.log(normalArray);
+
+}
+
+
+ if (Array.isArray([1, 3, 4], [])) {
+    originalArrays.splice(1, 0, 2);
+    console.log(originalArrays);
+    normalArray.splice(0, 0, 1);
+    normalArray.splice(1, 0, 2);
+    normalArray.splice(2, 0, 3);
+    normalArray.splice(3, 0, 4);
+    console.log(normalArray);
+ }
+
+
+
+  if (Array.isArray([1, 2, 3, 4], [1, 2, 3, 4])) {
+    originalArrays.splice(0, 4);
+    console.log(originalArrays);
+    normalArray.splice(0, 4);
+    console.log(normalArray);
+   }
+
+   
+ if (Array.isArray([]), ([])) {
+    originalArrays.splice(0, 0, "hey");
+    originalArrays.splice(1, 0, 2);
+    originalArrays.splice(2, 0, 3);
+    originalArrays.splice(4, 0, "ho");
+    console.log(originalArrays);
+    normalArray.splice(0, 0, "hey");
+    normalArray.splice(1, 0, 3);
+    console.log(normalArray);
+   }
+
+   if (Array.isArray(["hey", 2, 3, "ho"], ["hey", 3])) {
+ // hey 2 3 ho
+originalArrays.splice(0, 1);
+originalArrays.splice(1, 1);
+normalArray.splice(0, 2);
+console.log(normalArray);
+console.log(originalArrays);
+}
+
+
+if (Array.isArray([2, "ho"], [])) {
+    originalArrays.splice(0, 0, 1);
+originalArrays.splice(2, 1, 3);
+normalArray.splice(0, 0, "1");
+normalArray.splice(0, 0, 3);
+console.log(normalArray);
+console.log(originalArrays);
+}
+  
+
+if (Array.isArray([1, 2, 3], ["1", 3])) {
+  //123
+ originalArrays.splice(2, 1);
+ normalArray.splice(0, 2);
+ console.log(normalArray);
+ console.log(originalArrays);
+
+}
+
+}
+console.log(originalArrays);
+
+});
  
  
 
@@ -47,9 +182,11 @@ return originalArrays;
 
 
 }
+
+removeFromArray();
 
 
 
 // Do not edit below this line
 
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
