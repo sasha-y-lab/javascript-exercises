9f9950e0d1c2a507959b82e2e5aad7da1694e78f
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-06 10:09:10 -0400

    Edit js files but not working now

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,238 +3,55 @@
 
 const removeFromArray = function(arrays, ...args) { // // arrays, ...remove
    
-   
+  /* 
   let index = 2;
  const originalArrays = [1, 2, 3, 4];
  const removedArrays = [...originalArrays.slice(0, index), ...originalArrays.slice(index + 1)];
   //const removedArrays = [].slice.call(remove);
   arrays = originalArrays;
   args = removedArrays;
-  
-  /*
-  originalArrays.splice(0, 0, 4);
-  //console.log(originalArrays);
-  originalArrays.splice(0, 0, 2);
-  //console.log(originalArrays);
-  originalArrays.splice(1, 0, 3);
-  //console.log(originalArrays);
-  originalArrays.splice(0, 0, 1);
+  */
+  let i = 2;
+  let index = i;
+  const originalArrays = [1, 2, 3, 4];
+let start = 2;
+
+   
+ 
+  arrays = originalArrays;
+  console.log(arrays);
+
+
+  arrays.forEach((removedArrays) => {
+     removedArrays = [...originalArrays.slice(start, index), ...originalArrays.slice(index + 1)];
+     console.log(removedArrays);
+     args = removedArrays;
+      console.log(arrays);
+      console.log(i);
+      console.log(args);
+ console.log(index);
+      if (arrays.length [i] === args) {
+        console.log(i);
+        arrays.splice(i, 0, i);
+        console.log(arrays);
+        i--;
+        console.log(i);
+        console.log(originalArrays);
+ console.log(args);
+      }
+
+
+    
+  });
   console.log(originalArrays);
-  //normalArray.splice(0, 0, 3);
-  //console.log(normalArray);
-*/
-
- // for (let i = 0; i < originalArrays.length; i++) {
-  //for (const originalArray of originalArrays) {
-//originalArrays.forEach((originalArray) => {
-  //removedArrays.splice(2, 1);
-  //console.log(removedArrays);
-
-
-
-
-  //for (let i = 0; i >= originalArrays.length; i++) {
-
-   
-// if (Array.isArray([1, 2, 3, 4],[3])) {
+  console.log(args);
   
-   // originalArrays.splice(2, 1);
+  return args;
   
-// normalArray.splice(0, 1);
- 
- //console.log(originalArray);
- console.log(originalArrays);
- console.log(removedArrays);
- 
- return removedArrays;
- 
-}
-/*
- // its 124 = 14
- originalArrays.splice(1, 1);
- console.log(originalArrays);
- // 14 = 13
- originalArrays.splice(1, 1, 3);
- console.log(originalArrays);
- // 13 = 1234
- originalArrays.splice(1, 0, 2);
- originalArrays.splice(3, 0, 4);
- console.log(originalArrays);
-// 1234 = 134
-originalArrays.splice(1, 1);
-console.log(originalArrays);
-//134 = []
-originalArrays.splice(0, 3);
-console.log(originalArrays);
-//[] = 2 ho
-originalArrays.splice(0, 0, 2);
-originalArrays.splice(1, 0, "ho");
-console.log(originalArrays);
-// 2 ho = 12
-originalArrays.splice(1, 1);
-originalArrays.splice(0, 0, 1);
-console.log(originalArrays);
- 
-
-*/
-
-//console.log(originalArrays);
-
-
-
-/*
-  if (Array.isArray([1, 2, 4], [])) {
-
- originalArrays.splice(2, 0, 3)
- //console.log(originalArrays);
- normalArray.splice(0, 0, 3);
- normalArray.splice(0, 0, 2);
- //console.log(normalArray);
-  } 
-
-
-
-  if (Array.isArray([1, 2, 3, 4], [3, 2])) {
- originalArrays.splice(1, 2)
- //console.log(originalArrays);
- normalArray.splice(0, 2);
- //console.log(normalArray);
-}
-
-
-
-  if (Array.isArray([1, 4], [])) {
-
- originalArrays.splice(1, 1, 3);
- originalArrays.splice(1, 0, 2);
- originalArrays.splice(1, 0, 2);
- normalArray.splice(0, 0, 2);
- //console.log(normalArray);
- //console.log(originalArrays);
  }
 
-
-  if (Array.isArray([1, 2, 2, 3], [2])) {
-  originalArrays.splice(1, 2);
-  //console.log(originalArrays);
-  normalArray.splice(0, 1);
- //console.log(normalArray);
-   
- }
-
-
- if (Array.isArray([1, 3], [])) {
- 
-
-originalArrays.splice(1, 0, 2);
-originalArrays.splice(3, 0, 4);
-normalArray.splice(0, 0, 7);
-normalArray.splice(0, 0, "tacos");
-// console.log(normalArray);
-//console.log(originalArrays);
- } 
-
-
-
-if (Array.isArray([1, 2, 3, 4], [7, "tacos"])) {
-
-  normalArray.splice(0, 2);
-  //console.log(originalArrays);
-  //console.log(normalArray);
-  }
-
-
-  if (Array.isArray([1, 2, 3, 4], [])) {
-  //1234
-  normalArray.splice(0, 0, 7);
-  normalArray.splice(1, 0, 2);
-  //console.log(normalArray);
-  //console.log(originalArrays);
-}
-
-if (Array.isArray([1, 2, 3, 4], [7, 2])) {
-originalArrays.splice(1, 1);
-normalArray.splice(0, 2);
- // console.log(originalArrays);
- // console.log(normalArray);
-
-}
-
-
- if (Array.isArray([1, 3, 4], [])) {
-    originalArrays.splice(1, 0, 2);
-   // console.log(originalArrays);
-    normalArray.splice(0, 0, 1);
-    normalArray.splice(1, 0, 2);
-    normalArray.splice(2, 0, 3);
-    normalArray.splice(3, 0, 4);
-   // console.log(normalArray);
- }
-
-
-
-  if (Array.isArray([1, 2, 3, 4], [1, 2, 3, 4])) {
-    originalArrays.splice(0, 4);
-   // console.log(originalArrays);
-    normalArray.splice(0, 4);
-   // console.log(normalArray);
-   }
-
-   
- if (Array.isArray([]), ([])) {
-    originalArrays.splice(0, 0, "hey");
-    originalArrays.splice(1, 0, 2);
-    originalArrays.splice(2, 0, 3);
-    originalArrays.splice(4, 0, "ho");
-   // console.log(originalArrays);
-    normalArray.splice(0, 0, "hey");
-    normalArray.splice(1, 0, 3);
-    //console.log(normalArray);
-   }
-
-   if (Array.isArray(["hey", 2, 3, "ho"], ["hey", 3])) {
- // hey 2 3 ho
-originalArrays.splice(0, 1);
-originalArrays.splice(1, 1);
-normalArray.splice(0, 2);
-//console.log(normalArray);
-//console.log(originalArrays);
-}
-
-
-if (Array.isArray([2, "ho"], [])) {
-    originalArrays.splice(0, 0, 1);
-originalArrays.splice(2, 1, 3);
-normalArray.splice(0, 0, "1");
-normalArray.splice(0, 0, 3);
-//console.log(normalArray);
-//console.log(originalArrays);
-}
   
-
-if (Array.isArray([1, 2, 3], ["1", 3])) {
-  //123
- originalArrays.splice(2, 1);
- normalArray.splice(0, 2);
- //console.log(normalArray);
- //console.log(originalArrays);
-
-}
-*/
-//}
-//console.log(originalArrays);
-
-
- 
-
-
-//}
-
-  //}
-
-
-
-//}
+  
 
 //removeFromArray();
 
