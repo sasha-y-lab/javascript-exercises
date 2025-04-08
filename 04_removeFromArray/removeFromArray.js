f269ed8e7f5ad57b26d2886480247db18b731429
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-08 04:54:59 -0400

    Change around js code but only passing test 2

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -28,30 +28,47 @@ const removeFromArray = function(arrays, ...args) { // // arrays, ...remove
   console.log(args);
   console.log(arrays);
 
-removedArrays.splice(2, 1);
-console.log(args);
-console.log(arrays);
 
-  for (i = arrays.length - 1; i >= 0; i--) { // you are looping backwards to avoid shifting. 
+  
+  
   const allElements = (array) => array >= 1 && array <= 4;
   if (arrays.some(allElements)) { // this is true. the array = numbers 1 - 4
+  
+ 
+
+      
+    for (let i = arrays.length - 1; i < 5; i--) { // you are looping backwards to avoid shifting. 
+      
+      console.log(i);
+      args.splice(...args.filter(array => array !== i-- - 2));
+    
     console.log(i);
-    
-    
-    args.splice(...arrays.filter(array => array !== i++ + 2));
-    
    
     console.log(arrays);
     console.log(args);
   
+    for (let j = 0; j < 5; j++) {
+      console.log(i);
+      args.splice(...args.filter(array => array !== i-- - 2));
     
+    console.log(i);
+   
+    console.log(arrays);
+    console.log(args);
+    return args;
+
+    } // end of second for loop
+    console.log(arrays);
+    console.log(args);
 return args;
       
-    } // end of if
+    } // end of for loop
     console.log(args);
 console.log(arrays);
 return args;
-  } //end of for loop
+
+ 
+  } //end of if
   console.log(args);
   console.log(arrays);
   return args;
