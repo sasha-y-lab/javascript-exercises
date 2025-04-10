021469912479ce07e9223b223bdd483ff8f6b6cd
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-10 17:45:30 -0400

    Edit js to pass 1st test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,31 @@
-const leapYears = function() {
+const leapYears = function(year) {
+// Create a function that determines whether or not a given year is a leap year.
+// Leap years are determined by the following rules:
+// Leap years are years divisible by four with no remainder. However, 
+// years divisible by 100 are not leap years unless 
+// they are divisible by 400
 
+let result;
+
+// if year is divisible by 4 and has no remainder
+if (year % 4 === 0) {
+ 
+    result = true;
+
+}
+
+if (year % 100 === 0) {
+    result = false;
+
+    
+} else if (year % 100 === 0 && year % 400 === 0) {
+    result = true;
+}
+ return result;
 };
+
+leapYears(1996); // true
+
 
 // Do not edit below this line
 module.exports = leapYears;
