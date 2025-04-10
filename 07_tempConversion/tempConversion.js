e84e2cd93441591267de520a8db8ec9a5fc3c654
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-10 18:40:49 -0400

    Edit js to pass 2 first tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,33 @@
-const convertToCelsius = function() {
+//Write two functions that convert temperatures from Fahrenheit 
+// to Celsius, and vice versa:
+
+const convertToCelsius = function(degreeNumberFahr) {
+
+// what is the calculation to calculate farenheit to celcius
+// (x − 32) × ⁠(5/9) = degree C
+// round answer to 1 decimal point
+
+let celsius = (degreeNumberFahr - 32) * (5 / 9);
+
+let celsiusRounded = (Math.round(celsius * 10) / 10);
+return celsiusRounded;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeNumberCel) {
+// what is the calculation to calculate celcius to farenheit
+// (x) x (9/5) + 32 = degree f
+// round answer to 1 decimal point
+
+let fahrenheit = (degreeNumberCel * (9 / 5) + 32);
+let fahrenheitRound = (Math.round(fahrenheit * 10) / 10);
+return fahrenheitRound;
+
 };
+
+convertToCelsius(32); // returns 0
+convertToFahrenheit(0); // returns 32
 
 // Do not edit below this line
 module.exports = {
