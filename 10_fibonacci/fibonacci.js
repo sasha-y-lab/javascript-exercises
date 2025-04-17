980763b7568962a578075d5ca849dafdb3bb8fb3
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-16 22:43:31 -0400

    Edit js to pass the first test without using solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    
+    // needs to be an array
+
+    let num1;
+    let num2;
+
+    num1 = num2 = 1;
+
+    
+
+    for (let f = 1; f < n; f++) {
+        let fibNum = ((num1 - 1) + (num2 - 2)) + n;
+console.log(n);
+console.log(f);
+console.log(fibNum);
+ return fibNum;
+
+    }
+
+  // fibonnaci = (num1 - 1) + (num2 - 2) and is is valid for num > 2
+  // also for this, num1 = num2 = 1
+  // fibonnaci for this starts at 1.
+
+
 
 };
 
