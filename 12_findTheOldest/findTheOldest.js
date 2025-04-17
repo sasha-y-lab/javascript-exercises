316c0ab86f0bccde5642a38df4c4fcfdcf0d5b78
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-17 19:37:11 -0400

    Edit js to pass all tests
    Add solution with notes as death and birth object keys couldn't be read
    Note sure why they could not be read

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,3 +1,4 @@
+/*
 const getAge = function (birth, death) {
     if (!death) {
       death = new Date().getFullYear();
@@ -15,9 +16,14 @@ const getAge = function (birth, death) {
       return oldestAge < currentAge ? currentPerson : oldest;
     });
   }; 
+  */
  
- /* const getAge = function (birth, death) {
+  // i dont get why birth and death are read and yearOfBirth and yearOfDeath aren't read
+ const getAge = function (birth, death) {
         
+//birth = people.yearOfBirth; undefined is people
+//death = people.yearOfDeath; undefined is people
+
     if (!death) {
       death = new Date().getFullYear();
     }
@@ -26,52 +32,34 @@ const getAge = function (birth, death) {
   
 
 
- // sort the array in ascending order by value does same as map
- // function compareAges(a, b) {
-    
-  //  return a - b;
- // }
-
  
-
 const findTheOldest = function(people) {
 
     
-   // const peopleAges = people.map(person => getAge(person));
-   const peopleAges = people.reduce((oldest, currentPerson) => {
+   let peopleAges = people.reduce((oldest, currentPerson) => { // oldest is the first element which is Carly
+    console.log(oldest); //yes this is carly
+    
     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
     const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
-    oldestAge < currentAge ? currentPerson : oldest;
+   
+   console.log(oldestAge); // carly's age
+
+   if (oldestAge < currentAge) {
+ return currentPerson;
+
+   } else {
+    return oldest;
+   }
 
    });
 
         console.log(peopleAges);
      
-   // const sortAges = peopleAges.sort(compareAges); // sorts ages but does the same thin as get age map
-   //const sortAges = peopleAges.sort();
-   //  console.log(sortAges);
-
-      // get the highest number
-
-   // const oldestPerson = sortAges.map((people) => {
-//return { people[1] };
-
-   // });
-    //  console.log(oldestPerson);
-     // console.log(oldestPerson.name); // undefined
-     // console.log(oldestPerson.person); // undefined
-     // console.log(oldestPerson.person.name); // cannot read
-      // console.log(oldestPerson.people); //undefined
-     // console.log(oldestPerson.people.name); undefined name
-    //  console.log((oldestPerson).name); //undefined
-
-      //const finalOldest = oldestPerson.person.name;
-
-    // console.log(finalOldest);
+   
     return peopleAges;
 
 };
-*/
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
