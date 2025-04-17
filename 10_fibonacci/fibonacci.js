3c209e09621d4ca7e67fd370e6ae00b713e674f9
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-17 01:11:19 -0400

    Edit js to pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -7,9 +7,15 @@ const fibonacci = function(n) {
    //if (n === 0) return 0;
    //if (n === 1) return 1;
 
+   if (typeof n !== 'number') {
+    n = parseInt(n)
+};
+
+
+
    if (n === 1 || n === 2) return 1; // these are defined already, no need for calc
    if (n === 0) return 0;
-   
+   if (n < 0) return "OOPS";
 
 
    let num1;
