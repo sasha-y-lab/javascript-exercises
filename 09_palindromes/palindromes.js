a483fbde09d095c36cc4c164b92d4d667a19c171
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-16 21:24:46 -0400

    Edit to pass 6 tests, however using the solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,19 +1,27 @@
 const palindromes = function (string1) {
+let allAlpha = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+let toLowerCase = string1.toLowerCase();
 
-let splitString = string1.split("/^[a-z]+$/i");
+let splitString = toLowerCase.split("");
 
-let reverseArray = splitString.reverse();
+let filterAlpha = splitString.filter((character) => allAlpha.includes(character));
+
+let reverseArray = filterAlpha.reverse();
 
 let joinArray = reverseArray.join("");
 
-return string1 === joinArray;
+let reversedString = joinArray.split('').reverse().join('');;
 
+return joinArray === reversedString;
 
 };
 
+
+
 palindromes('racecar');
 palindromes('racecar!');
+
 
 
 // Do not edit below this line
