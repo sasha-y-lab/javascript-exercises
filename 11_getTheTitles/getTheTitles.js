fed22317243876316107c09420116cdcab94f690
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-17 01:38:38 -0400

    Edit js to pass test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+
+  
+   // return array.filter(books => books.title); // returns entire array
+
+   console.log(array.map(books => books.title));
+   return array.map(books => books.title); // gets just once instance of all objects for one key
+ 
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
