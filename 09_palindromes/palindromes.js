78c155e629f1a00b532914a0d406c6d190ee3ddb
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-16 21:34:10 -0400

    Edit js to solution as it wasn't woring with my solution to pass last 2 tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,28 +1,28 @@
 const palindromes = function (string1) {
-let allAlpha = 'abcdefghijklmnopqrstuvwxyz0123456789';
-
-let toLowerCase = string1.toLowerCase();
-
-let splitString = toLowerCase.split("");
-
-let filterAlpha = splitString.filter((character) => allAlpha.includes(character));
-
-let reverseArray = filterAlpha.reverse();
-
-let joinArray = reverseArray.join("");
-
-let reversedString = joinArray.split('').reverse().join('');;
-
-return joinArray === reversedString;
-
-};
-
-
-
-palindromes('racecar');
-palindromes('racecar!');
-
-
-
-// Do not edit below this line
-module.exports = palindromes;
+    let allAlpha = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    let toLowerCase = string1.toLowerCase();
+    
+    let splitString = toLowerCase.split("");
+    
+    let filterAlpha = splitString.filter((character) => allAlpha.includes(character));
+    
+    let reverseArray = filterAlpha.reverse();
+    
+    let joinArray = reverseArray.join("");
+    
+    let reversedString = joinArray.split('').reverse().join('');;
+    
+    return joinArray === reversedString;
+    
+    };
+    
+    
+    
+    palindromes('racecar');
+    palindromes('racecar!');
+    
+    
+    
+    // Do not edit below this line
+    module.exports = palindromes;
