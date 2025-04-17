a14fb9169722208d84ffc883c9ab181774738595
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-04-16 20:00:32 -0400

    Edit js to pass first test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string1) {
+
+let lowerCaseString = string1.toLowerCase();
+
+let splitString = lowerCaseString.split("");
+
+let reverseArray = splitString.reverse();
+
+let joinArray = reverseArray.join("");
+
+return string1 === joinArray;
+
 
 };
+
+palindromes('racecar');
+
 
 // Do not edit below this line
 module.exports = palindromes;
